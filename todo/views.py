41a0e28e7df6e2c2535bef8705feddc25ab38886
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-04-27 02:51:08 +0200

    views.py aktualisieren

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import TodoItem
+from .forms import TodoItemForm
 
 def todo_list(request):
     todos = TodoItem.objects.all()
@@ -7,22 +8,27 @@ def todo_list(request):
 
 def todo_create(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        TodoItem.objects.create(title=title)
-        return redirect('todo_list')
-    return render(request, 'todo_app/todo_create.html')
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoItemForm()
+    return render(request, 'todo_app/todo_create.html', {'form': form})
 
 def todo_update(request, todo_id):
-    todo = TodoItem.objects.get(id=todo_id)
+    todo = get_object_or_404(TodoItem, id=todo_id)
     if request.method == 'POST':
-        todo.title = request.POST.get('title')
-        todo.completed = request.POST.get('completed') == 'on'
-        todo.save()
-        return redirect('todo_list')
-    return render(request, 'todo_app/todo_update.html', {'todo': todo})
+        form = TodoItemForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoItemForm(instance=todo)
+    return render(request, 'todo_app/todo_update.html', {'form': form, 'todo': todo})
 
 def todo_delete(request, todo_id):
-    todo = TodoItem.objects.get(id=todo_id)
+    todo = get_object_or_404(TodoItem, id=todo_id)
     if request.method == 'POST':
         todo.delete()
         return redirect('todo_list')
